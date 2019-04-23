b8766cfa20d57d23b71705f4e7f8459d3f4d0558
minlucyzhao <minlucyzhao@gmail.com>
2019-04-23 11:42:01 +0000

    Add Tweet Date Indicator and edit comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -19,29 +19,4 @@ $(document).ready(function() {
         //OR $('.counter').text(showCount) (not recommended but works)
         //OR $(this).siblings('span').text(showCount) (also works)
     });
-
-    // $(".tweet-input").on('input', function() {
-    //     console.log($(this)); //Recognizes when you click away from section, the input event fires when the value of an <input>, <select>, or <textarea> element has been changed. 
-    // });
-
-    // $(".tweet-input").on('change', function() {
-    //     console.log("change"); //Recognizes once you type in the textarea and clicks away
-    // });
-
-    // $(".tweet-input").on('keyup', function() {
-    //     console.log("keyup"); //Recognizes when you release the key
-    // });
-
-    // $(".tweet-input").on('keydown', function() {
-    //     console.log("keydown"); //Recognizes when you press down the key
-    // });
-
-    // $(".tweet-input").on('blur', function() {
-    //     console.log("blur"); //Recognizes when you click away from section
-    // });
-
-    // $(".tweet-input").on('keypress', function() {
-    //     console.log("keypress"); //ANY key except Shift, Fn, CapsLock is in pressed position. (Fired continously.)
-    // });
-    
   });
